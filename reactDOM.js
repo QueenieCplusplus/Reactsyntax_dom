04a0f98ac6eebe822c082721a7d755cbce9f7b1a
Queenie Chen <58072875+QueenieCplusplus@users.noreply.github.com>
2020-07-10 13:36:04 +0800

    Add files via upload

---
+++ reactDOM.js
@@ -0,0 +1,49 @@
+//2020.7/10, pm 1:10 - 1: 35
+//react & js lib using DOM
+// Dom Node
+// using this.refs.domnode.getDOMNode()
+
+var React = require("react");
+var World = React.createClass({
+
+    // node
+    render: function(){
+
+        return <div ref="autoK"/>
+
+    },
+
+    // func
+    funcalled: function(w){
+        newdata = this.state.data.add(w);
+        alert(newdata);
+
+    },
+
+    // data
+    getInitialState: function(){
+        return
+        { data: ["20191222", "poupou", "cat", "rainbowBridge"]};
+
+    },
+
+    componentDidMount: function(){ //可能存在移除節點上元件後仍然存在元件的功能，造成記憶體洩漏
+
+        autodo({
+            target: this.refs.autoK.getDOMNode(),
+            data: this.state.data,
+            event: this.funcalled
+            // event: { listener: this.funcalled}
+
+        });
+
+    },
+
+    componentWillUnmount: function(){ //解決記憶體洩漏問題，在此生命週期中，將其DOM Node直接移除，可清理元件。
+
+        this.refs.autoK.getDOMNode().clear();
+        // this.res.autoK.getDOMNode().removeChild();
+
+    }
+
+});
